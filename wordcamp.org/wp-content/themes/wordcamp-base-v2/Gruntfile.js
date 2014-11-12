8d3711453f0b77c98c49b77a80b23c9ea8bec691
Daryn St. Pierre <daryn@bloqmail.com>
2014-11-12 09:17:06 -0500

    Rewrote the Gruntfile a bit to try and include SassDoc but it's throwing an error. Committing for now and will fix later. It might be related to Yosemite fucking up all of the things.

--- wordcamp.org/wp-content/themes/wordcamp-base-v2/Gruntfile.js
+++ wordcamp.org/wp-content/themes/wordcamp-base-v2/Gruntfile.js
@@ -1,57 +1,64 @@
 /*global module:false*/
 module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+		'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+		'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>',
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>',
-	sass: {
-		dist : {
-      options : {
-        require : [ 'bourbon', 'neat', 'sass-globbing' ]
-      },
-			files : [ {
+		sass: {
+			dist : {
+				options : {
+				require : [ 'bourbon', 'neat', 'sass-globbing' ]
+			},
+			files : [{
 				expand : true,
 				flatten : true,
 				cwd : 'scss',
 				src : ['**/*.scss', '!**/_*.scss'],
 				dest : './',
 				ext : '.css'
-			} ]
-		}
-	},
-    // Task configuration.
-    watch: {
-		dist : {
-			files: [
-				'scss/**/*.scss'
-			],
-			tasks: ['sass'],
-			options: {
-				livereload: true
+			}]
 			}
+		},
+
+		watch: {
+			dist : {
+				files: [ 'scss/**/*.scss' ],
+				tasks: ['sass'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+
+		cssmin : {
+			dist : {
+				files : {
+					'style.min.css' : [ 'style.css' ],
+				}
+			}
+		},
+
+		sassdoc : {
+			files : [{
+				src : [
+					'scss/**/*.scss',
+					'scss/**/_*.scss',
+					'!scss/bourbon',
+					'!scss/neat'
+				],
+				dest : 'docs'
+			}]
 		}
-    },
 
-    cssmin : {
-    	dist : {
-    		files : {
-    			'style.min.css' : [ 'style.css' ],
-    		}
-    	}
-    }
+	});
 
-  });
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-sassdoc');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-  // Default task.
-  grunt.registerTask('default', ['sass']);
-  grunt.registerTask('build', ['default','cssmin'])
+	grunt.registerTask('default', [ 'sass' ]);
+	grunt.registerTask('build', [ 'default','cssmin','sassdoc' ]);
 };
